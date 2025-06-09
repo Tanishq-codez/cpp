52587d6ee005d30e8511c7e1e5b442e0474fa29b
Tanishq-codez <chhabratanishq69@gmail.com>
2025-06-09 20:37:15 +0530

    dp

---
+++ lecture102/1into.cpp
@@ -0,0 +1,4 @@
+// dynamic progg : overlapping subproblems ; bigger problem sol = many smaller problems sol
+// top down approach - recurssion + memoization (store sol of subproblem in table / map)
+// bottom up approach - tabulation
+// + space optimization
